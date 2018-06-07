70d025dd1e6826e5fc7c683e581d9505afbbeb97
YunBum SUNG <yb.sung@lge.com>
2018-06-07 23:06:00 +0900

    3. SignInActivity: 로그인

--- java/com/test/edu/kotlin_fb_demo/MainActivity.kt
+++ java/com/test/edu/kotlin_fb_demo/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.test.edu.kotlin_fb_demo
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.signin.SignIn
 import com.test.edu.kotlin_fb_demo.ui.RootActivity
+import com.test.edu.kotlin_fb_demo.ui.SignInActivity
 
 class MainActivity : RootActivity() {
 
@@ -12,6 +15,8 @@ class MainActivity : RootActivity() {
 
         // 인증을 담당하는 액티비티로 이동
         // showProgressDialog()
+        startActivity( Intent(this, SignInActivity::class.java) )
+        finish()
     }
 
     // 백키를 누르면 앱이 종료되는데, 이 이벤트를 인터셉트
---
+++ java/com/test/edu/kotlin_fb_demo/ui/SignInActivity.kt
@@ -0,0 +1,13 @@
+package com.test.edu.kotlin_fb_demo.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.test.edu.kotlin_fb_demo.R
+
+class SignInActivity : RootActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
