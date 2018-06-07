884553247ccdd5eb647905237262c7d3ee8bf0af
YunBum SUNG <yb.sung@lge.com>
2018-06-07 23:04:52 +0900

    1. RootActivity.kt

---
+++ java/com/test/edu/kotlin_fb_demo/ui/RootActivity.kt
@@ -0,0 +1,16 @@
+package com.test.edu.kotlin_fb_demo.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+open class RootActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    // 로딩
+    // 로딩 닫기
+    // 로그아웃
+    // 세션값 획득
+    // 백키처리
+}
