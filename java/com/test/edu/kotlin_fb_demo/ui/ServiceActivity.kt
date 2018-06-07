d6dd3c33cc38681ec65500c189ab676b1098e66c
YunBum SUNG <yb.sung@lge.com>
2018-06-07 23:10:24 +0900

    7. RootFragment and page adapter

--- java/com/test/edu/kotlin_fb_demo/ui/ServiceActivity.kt
+++ java/com/test/edu/kotlin_fb_demo/ui/ServiceActivity.kt
@@ -1,27 +1,51 @@
 package com.test.edu.kotlin_fb_demo.ui
 
+
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import com.test.edu.kotlin_fb_demo.R
+import com.test.edu.kotlin_fb_demo.frag.RootFragment
 import kotlinx.android.synthetic.main.activity_service.*
 
 class ServiceActivity : RootActivity() {
+    // 뷰페이저의 아답터 (뷰프레그먼트아답터)
+    private var mPagerAdapter:FragmentPagerAdapter
+
+    init {
+        mPagerAdapter = object :FragmentPagerAdapter(supportFragmentManager) {
+            private val mFragments = arrayOf<Fragment>(RootFragment(), RootFragment(), RootFragment())
+
+            // 뷰페이저가 요청하는 특정 화면
+            override fun getItem(position: Int): Fragment {
+                return mFragments[position]
+            }
+
+            // 뷰페이저에서 보옂저야할 총 화면 개수
+            override fun getCount(): Int {
+                return mFragments.size
+            }
+
+            // 프레그먼티의 제목을 지정할 경우
+            override fun getPageTitle(position: Int): CharSequence? {
+                return ""
+            }
+        }
+    }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -33,6 +57,8 @@ class ServiceActivity : RootActivity() {
         setContentView(R.layout.activity_service)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        // 뷰페이저에 화면 공급자 연결
+        viewPager.adapter = mPagerAdapter
     }
 
     // 로그아웃 기능, 글쓰기 기능 넣기
